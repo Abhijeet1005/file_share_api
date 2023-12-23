4b522fccbe5d64b7aadbb1ce79829481f5068572
Abhijeet Chauhan <aschauhan1052002@gmail.com>
2023-12-23 18:35:06 +0530

    Base commit, upload working

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+const upload = multer({storage})
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', upload.single('file'),function(req, res, next) {
+  if(!req.file) return res.status(400).json({error: "No file uploaded"})
+  res.json({message: "File uploaded successfully", filename: req.file.filename})
+});
+
+module.exports = router;
