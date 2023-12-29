d37cc110dd0dbc1114478f22fa23a3c822d50f0a
Abhijeet Chauhan <aschauhan1052002@gmail.com>
2023-12-29 15:04:11 +0530

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const multer = require('multer');
+const fs = require('fs');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -15,6 +16,8 @@ const upload = multer({storage})
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  console.log(fs.readdirSync('./'))
+  
 });
 
 router.post('/', upload.single('file'),function(req, res, next) {
